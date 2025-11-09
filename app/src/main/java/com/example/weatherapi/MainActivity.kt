996598a0f06784ead29deefa6a98f4be50aa1f99
Javeria Jalil <javeriajalil2@gmail.com>
2025-11-09 13:24:37 -0500

    ready for merge1, deleted SplashScreen.kt, that logic is implemented in LoginScreen.kt, doing first merge with Shan and I 's branch

--- app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapi
 import LoginScreen
+import SplashScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
